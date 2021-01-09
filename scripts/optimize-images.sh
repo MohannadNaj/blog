d0114b34ac4cb849907645420099fa98aa6d36ef
Mohannad Najjar <mohannadnaj@me.com>
2021-01-09 06:52:42 +0300

    Add optimize-images script

---
+++ scripts/optimize-images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+images=$(find -E static/posts -regex ".*/.*.(png|jpeg)")
+
+total=$(echo "$images" | wc -l)
+num=0
+
+echo "There are $total files to be converted."
+
+for f in $images
+do
+    ((num++))
+    echo "Converting $f, $num / $total"   
+    #filename=$(basename -- $f | sed 's/\.[^.]*$//')
+    dest="${f/posts/posts-optimized}"
+    parentdir="$(dirname "$dest")"
+    mkdir -p $parentdir
+    convert "./$f" -blur 13x20 -filter LanczosRadius -resize 105%x105% \( "$f" -gravity center +repage \) -composite -compose screen "$dest"
+    convert "$dest" -resize 700\> "$dest"
+#    clear
+
+done
